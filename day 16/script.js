3894f1d0e5d2951fd550293cda765bef32f5dc62
Arju Khadka <arjukhadka13@gmail.com>
2025-11-10 16:47:49 +0545

    intro to js

---
+++ day 16/script.js
@@ -0,0 +1,20 @@
+//  console.log("Hello world")
+
+var cup = 'coffee'
+var collegeBag = 'book'
+var noOfChauchau = '10'
+
+cup = 10
+
+console.log(cup)
+console.log(collegeBag)
+
+// let cup = 'coffee'
+// let collegeBag = 'book'
+// let noOfChauchau = '10'
+
+// const cup = 'coffee'
+// const collegeBag = 'book'
+// const noOfChauchau = '10'
+
+
