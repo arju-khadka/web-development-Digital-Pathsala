fad696068d7cf8fb88127d41358b32326ff2e543
Arju Khadka <arjukhadka13@gmail.com>
2025-11-11 16:13:26 +0545

    Variables,arrays and objects in js

---
+++ day 17/script.js
@@ -0,0 +1,31 @@
+var name1 = "Arju Khadka"
+var name2 = "Manish Basnet"
+var name3 = "Bob Marley"
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
+
+
+//Array
+var names = ['Arju Khadka','Manish Basnet', 'Bob Marley']
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+
+names.pop()
+
+//Object
+var person1 = {
+    name : "Arju khadka",
+    age : 21,
+    address : "Damak"
+}
+
+person1.name = "Bob Marley"
+delete person1.age
+console.log(person1.name)
+console.log(person1.age)
+console.log(person1.address)
+
+console.log(person1.name,person1.age,person1.address)
