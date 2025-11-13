aa1dc9f2faaabb111980f817976366ca18dcf8be
Arju Khadka <arjukhadka13@gmail.com>
2025-11-13 16:58:41 +0545

    loops in js

---
+++ day 19/script.js
@@ -0,0 +1,37 @@
+// for(var i=0; i<100; i=i+5){
+//     console.log( i,"I am Arju Khadka")
+// }
+
+// var i = 0
+// while(i<10){
+//     console.log("I am Arju")
+//     i++
+// }
+
+// let i=0
+// do{
+//     console.log("I am arju")
+//     i++
+// }
+// while(i<10)
+   
+
+var person={
+    name : "Arju Khadka",
+    age : 21,
+    nationality : "Nepali",
+    address : "Damak"
+}
+
+// for(let key in person){
+//     console.log(key,person[key])
+// }
+
+var names = ["Arju","Ram","Sita"]
+// for(let index in names){
+//     console.log(names[index])
+// }
+
+for(let name of names){
+    console.log(name)
+}
