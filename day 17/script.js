779d2133473c7396a0e85cd7a909ec3330f213a2
Arju Khadka <arjukhadka13@gmail.com>
2025-11-13 15:09:12 +0545

    Conditional Statement

--- day 17/script.js
+++ day 17/script.js
@@ -2,18 +2,19 @@ var name1 = "Arju Khadka"
 var name2 = "Manish Basnet"
 var name3 = "Bob Marley"
 
-console.log(name1)
-console.log(name2)
-console.log(name3)
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
 
 
 //Array
 var names = ['Arju Khadka','Manish Basnet', 'Bob Marley']
-console.log(names[0])
-console.log(names[1])
-console.log(names[2])
+console.log(names.includes('Arju Khadka'))
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
 
-names.pop()
+// names.pop()
 
 //Object
 var person1 = {
@@ -22,10 +23,10 @@ var person1 = {
     address : "Damak"
 }
 
-person1.name = "Bob Marley"
-delete person1.age
-console.log(person1.name)
-console.log(person1.age)
-console.log(person1.address)
+// person1.name = "Bob Marley"
+// delete person1.age
+// console.log(person1.name)
+// console.log(person1.age)
+// console.log(person1.address)
 
-console.log(person1.name,person1.age,person1.address)
+// console.log(person1.name,person1.age,person1.address)
---
+++ day 18/script.js
@@ -0,0 +1,22 @@
+// var age = 19 
+
+// if(age == 19 || age==21){
+//     console.log("You can vote")
+// }
+// else{
+//     console.log("You cannot vote")
+// }
+
+var age = 18
+if(age >= 18){
+    console.log("You are a college student.")
+}
+
+else if(age >= 5) {
+    console.log("You are school student.")
+}
+
+else
+    {
+    console.log("You are a montessori student.")
+}
