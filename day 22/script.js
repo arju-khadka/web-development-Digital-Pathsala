14f8c360efc7906d093839979d043eb7f5a7caf4
Arju Khadka <arjukhadka13@gmail.com>
2025-11-17 19:15:50 +0545

    DOM

---
+++ day 22/script.js
@@ -0,0 +1,17 @@
+// console.log("hello")
+
+// const h1 = document.querySelector('.catchMe')
+const h1 = document.getElementsByClassName('catchMe')
+// h1.textContent = "Learning C"
+// h1.style.color = "blue"
+
+// console.log(h1)
+
+const button = document.querySelector('.clickMe')
+button.addEventListener("click",function(){
+    h1.textContent = "Learning Click Me"
+})
+
+function changeTextContent(){
+    console.log("Clicked on button")
+}
